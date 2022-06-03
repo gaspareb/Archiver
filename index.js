d13decc6c4489e53e5eef17969b4c9183b5ccb87
gaspareb <gaspareb@yahoo.com>
2022-06-03 14:43:27 -0400

    added account info getting

--- index.js
+++ index.js
@@ -5,12 +5,63 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const contentType=process.env.S3_CONTENT_TYPE
 const aws = require('aws-sdk');
+const { Pool, Client } = require('pg');
+
+const credentials = {
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+};
+
+
+function getAccounts(){
+ 
+}
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/getAccounts', (req, res) => {
+    const pool = new Pool({
+      user: 'euualwhhhuwexs',
+      host: 'ec2-34-230-153-41.compute-1.amazonaws.com',
+      database: 'd2ge3qgch9nreg',
+      password: '809ea4681dc421d11105a939afc1524a7770ad760fbc8593fddcd3f3becfdbee',
+      port: 5432,
+      ssl: { rejectUnauthorized: false }  
+    })
+    var theaccount = "";
+    var accountNo = "";
+    var Industry = "";
+    var o = {} // empty Object
+    const results = pool.query('SELECT * from salesforce.account ORDER BY NAME ASC', (err, resp) => {            
+      pool.end();
+      if(err){
+        console.log('err ' + Date.now() + ' ' + err);
+      }else{    
+        // console.log('JSON.stringify(data) ' + JSON.stringify(resp));      
+        var key = 'Accounts';
+        o[key] = []; // empty Array, which you can push() values into
+        for(let i = 0; i < resp.rows.length; i++){       
+          theaccount = resp.rows[i].name;
+          accountNo = resp.rows[i].accountnumber;
+          Industry = resp.rows[i].industry;
+          var data1 = {
+            theaccount: theaccount, 
+            accountNo: accountNo,
+            Industry: Industry
+          };
+          o[key].push(data1); 
+        }      
+      }
+      res.write(JSON.stringify(o) + "\n");
+      res.end(); 
+    })  
+  })
   .get('/listBuckets', (req, res) => {
     aws.config.update({
       accessKeyId: process.env.S3_ACCESS_KEY_ID,
@@ -54,7 +105,7 @@ express()
     });
     }
   )
-  .get('/archiver', (req, res) => {
+  .get('/archiver', async (req, res) => {
         const date = new Date();    
         console.log('A new request received at ' + date + ' ' + process.env.S3_BUCKET); 
         //get data
